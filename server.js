e83695e7786e9cb36fc9a209cae02c07a392333f
stableShip <296888680@qq.com>
2016-12-08 17:25:54 +0800

    添加 基础 通讯程序, 可正常通讯

---
+++ server.js
@@ -0,0 +1,61 @@
+var thrift = require("thrift");
+var Calculator = require("./gen-nodejs/Calculator");
+var ttypes = require("./gen-nodejs/tutorial_types");
+var SharedStruct = require("./gen-nodejs/shared_types").SharedStruct;
+
+var data = {};
+
+var server = thrift.createServer(Calculator, {
+    ping: function() {
+        console.log("ping()");
+    },
+
+    add: function(n1, n2) {
+        console.log("add(", n1, ",", n2, ")");
+        return n1 + n2;
+    },
+
+    calculate: function(logid, work) {
+        console.log("calculate(", logid, ",", work, ")");
+
+        var val = 0;
+        if (work.op == ttypes.Operation.ADD) {
+            val = work.num1 + work.num2;
+        } else if (work.op === ttypes.Operation.SUBTRACT) {
+            val = work.num1 - work.num2;
+        } else if (work.op === ttypes.Operation.MULTIPLY) {
+            val = work.num1 * work.num2;
+        } else if (work.op === ttypes.Operation.DIVIDE) {
+            if (work.num2 === 0) {
+                var x = new ttypes.InvalidOperation();
+                x.whatOp = work.op;
+                x.why = 'Cannot divide by 0';
+                throw x;
+            }
+            val = work.num1 / work.num2;
+        } else {
+            var x = new ttypes.InvalidOperation();
+            x.whatOp = work.op;
+            x.why = 'Invalid operation';
+            throw x;
+        }
+
+        var entry = new SharedStruct();
+        entry.key = logid;
+        entry.value = ""+val;
+        data[logid] = entry;
+        return val;
+    },
+
+    getStruct: function(key) {
+        console.log("getStruct(", key, ")");
+        return data[key];
+    },
+
+    zip: function() {
+        console.log("zip()");
+    }
+
+});
+
+server.listen(9090);
